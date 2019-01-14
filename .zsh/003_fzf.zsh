dda04fd94a3065a1ce2cbd910fe941555a80bc35
tdaira <asada09403@gmail.com>
2019-01-14 21:56:42 +0900

    Update fzf options.

--- .zsh/003_fzf.zsh
+++ .zsh/003_fzf.zsh
@@ -8,33 +8,21 @@ zstyle ':chpwd:*' recent-dirs-default true
 zstyle ':chpwd:*' recent-dirs-file "$HOME/.chpwd-recent-dirs"
 zstyle ':chpwd:*' recent-dirs-pushd true
 
-# fr - interactive cdr
-function fr() {
+# interactive cdr
+function interactive-cdr() {
     target_dir=`cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf`
     target_dir=`echo ${target_dir/\~/$HOME}`
     if [ -n "$target_dir" ]; then
-        cd $target_dir
+        BUFFER="cd ${target_dir}"
+        zle accept-line
     fi
 }
+zle -N interactive-cdr
+bindkey '^rd' interactive-cdr
 
-# fh - repeat history
-fh() {
+# repeat history
+function repeat-history() {
   eval $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac | sed 's/ *[0-9]* *//')
 }
-
-# fd - cd to selected directory
-fd() {
-    local dir
-        dir=$(find ${1:-.} -path '*/\.*' -prune \
-                -o -type d -print 2> /dev/null | fzf +m) &&
-    cd "$dir"
-}
-
-# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-fbr() {
-    local branches branch
-        branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
-        branch=$(echo "$branches" |
-                fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-        git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
+zle -N repeat-history
+bindkey '^rh' repeat-history
